37888b72c296d4dffd1814628e1ae0868e9bb95c
Leo Ruiz <ruizruizleo@hotmail.com>
2022-02-22 07:39:45 -0300

    Create codigo.ino

---
+++ Aula1/codigo.ino
@@ -0,0 +1,25 @@
+const int botao1 = 2;
+const int botao2 = 3;
+
+int var1;
+int var2;
+  
+ void setup()
+ { 
+    pinMode(botao1, INPUT);
+  	pinMode(botao2, INPUT); 
+   	Serial.begin(9600);
+ }
+ 
+ void loop()
+ {
+    var1 = digitalRead(botao1);
+   	var2 = digitalRead(botao2);
+   	
+   if(var1){
+     Serial.println("botao 1 apertado");
+   }
+   if(!var2){
+     Serial.println("botao 2 apertado");
+   }
+ }
